743f4876f5875af9a98c0ff2763133cfcadc30b6
saraelsheemi <saraelsheemi93@yahoo.com>
2016-12-17 16:12:46 +0200

    android project

---
+++ E-Lib/app/src/main/java/com/example/win81/e_lib/savedmaterial.java
@@ -0,0 +1,13 @@
+package com.example.win81.e_lib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class savedmaterial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+}

d0b45dca1b4c2dc652aee7843c6bdb1a20de7770
HanaAhmed <hanasalah864@yahoo.com>
2016-12-19 16:09:32 +0200

    update

--- E-Lib/app/src/main/java/com/example/win81/e_lib/SignOut.java
+++ E-Lib/app/src/main/java/com/example/win81/e_lib/SignOut.java
@@ -3,6 +3,11 @@ package com.example.win81.e_lib;
 /**
  * Created by hanahana on 19/12/2016.
  */
-public class SignOut {
+public class SignOut extends HomeCommands{
 
+
+    @Override
+    public void excute() {
+
+    }
 }

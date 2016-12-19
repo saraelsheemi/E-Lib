2d053a8b8efee2526ebed53ba9e3d3378815fe7e
HanaAhmed <hanasalah864@yahoo.com>
2016-12-19 14:56:43 +0200

    test

---
+++ E-Lib/E-Lib/app/src/main/java/com/example/win81/e_lib/OpenSavedPage.java
@@ -0,0 +1,11 @@
+package com.example.win81.e_lib;
+
+/**
+ * Created by tabda on 12/18/2016.
+ */
+
+public class OpenSavedPage extends HomeCommands {
+    public static void excute(){
+
+    };
+}
---
+++ E-Lib/E-Lib/app/src/main/java/com/example/win81/e_lib/UploadRequestView.java
@@ -0,0 +1,9 @@
+package com.example.win81.e_lib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadRequestView extends AppCompatActivity {
+
+    
+}

98ee38307de6fc1c872dbc3c34de0fdb441b29dc
HagarGadallah <hagargadallah@gmail.com>
2016-12-19 16:41:20 +0200

    d

--- E-Lib/E-Lib/app/src/main/java/com/example/win81/e_lib/UploadRequestView.java
+++
@@ -1,9 +0,0 @@
-package com.example.win81.e_lib;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class UploadRequestView extends AppCompatActivity {
-
-    
-}
